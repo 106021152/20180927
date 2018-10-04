5c3fcf5e2655f5689f6c82f1c12b535359d39325
106021152 <32092982+106021152@users.noreply.github.com>
2018-10-04 11:52:28 +0800

    Update Success.java

--- src/Success.java
+++ src/Success.java
@@ -1,13 +1,46 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Success extends JFrame {
-    private JLabel jlb = new JLabel("成功");
+    private JMenuBar jmb = new JMenuBar();
+    private JMenu jmF =new JMenu("File");
+    private JMenu jmT = new JMenu("Tool");
+    private JMenu jmG = new JMenu("Game");
+    private JMenu jmH = new JMenu("Help");
+    private JMenuItem jmiOOXX = new JMenuItem("OOXX");
+    private JMenuItem jmiOpen = new JMenuItem("Open");
+    private JMenuItem jmiClose = new JMenuItem("Close");
+    private JMenuItem jmiExit = new JMenuItem("Exit");
     public Success(){
         init();
     }
     public void init(){
-        this.setBounds(0,0,200,200);
-        this.add(jlb);
-        jlb.setBounds(0,0,50,50);
+        this.setBounds(100,100,800,600);
+        this.setLayout(null);
+        this.setJMenuBar(jmb);
+        jmb.add(jmF);
+        jmb.add(jmT);
+        jmb.add(jmG);
+        jmb.add(jmH);
+        jmG.add(jmiOOXX);
+        jmF.add(jmiOpen);
+        jmF.add(jmiClose);
+        jmF.add(jmiExit);
+        jmiExit.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        jmiOOXX.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OOXX ox = new OOXX();
+                ox.setVisible(true);
+                Success.this.setVisible(false);
+            }
+        });
     }
 }
