33d45a324e83aeef213bb00bcaaac8e0b66a907c
106021152 <32092982+106021152@users.noreply.github.com>
2018-10-05 14:41:44 +0800

    Update Encrypt.java

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -1,35 +1,55 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Encrypt extends JFrame {
-    private JTextArea jtL = new JTextArea();
-    private JTextArea jtR = new JTextArea();
-    private JScrollPane jsL = new JScrollPane(jtL);
-    private JScrollPane jsR = new JScrollPane(jtR);
-    private JRadioButton jrdbE = new JRadioButton("Encrypt");
-    private JRadioButton jrdbD = new JRadioButton("Decrypt");
-    private String [] methodst ={"Caesar","XOR"};
-    private JButton jbtnRun = new JButton("Run");
-    private JButton jbtnClose = new JButton("Close");
-    private JPanel jpnC = new JPanel(new GridLayout(4,4,4,4));
-    private JPanel jpnW = new JPanel();
-    private JPanel jpnN = new JPanel();
-    private Container cp;
+    private JLabel jlbM = new JLabel("Method");
+    private JLabel jlbK = new JLabel("Key");
+    private JTextArea jaL = new JTextArea("",30,15);
+    private JTextArea jaR = new JTextArea("",30,15);
+    private JScrollPane jsL = new JScrollPane(jaL);
+    private JScrollPane jsR = new JScrollPane(jaR);
+    private JPasswordField jpf = new JPasswordField();
+    private String [] methodstr = {"Caesar","Xor"};
+    private JComboBox jcb = new JComboBox(methodstr);
+    private JRadioButton jbtnE = new JRadioButton("Encrypt");
+    private JRadioButton jbtnD = new JRadioButton("Decrypt");
+    private JButton jbtnR = new JButton("Run");
+    private JButton jbtnC = new JButton("Close");
+    private JPanel jpnC = new JPanel(new GridLayout(8,1,2,2));
+    private JPanel jpnL = new JPanel();
+    private JPanel jpnR = new JPanel();
+    Container cp;
+    ButtonGroup BG = new ButtonGroup();
     public Encrypt(){
         init();
     }
     public void init(){
-        this.setBounds(100,100,600,400);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setBounds(100,100,500,500);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         cp = this.getContentPane();
         cp.add(jpnC,BorderLayout.CENTER);
-        cp.add(jpnW,BorderLayout.WEST);
-        cp.add(jpnN);
-        cp.add(jpnW);
-        jpnC.add(jrdbE);
-        jpnC.add(jrdbD);
-        jpnC.add(jbtnRun);
-        jpnC.add(jbtnClose);
-        jpnW.add(jsL);
+        cp.add(jpnL,BorderLayout.WEST);
+        cp.add(jpnR,BorderLayout.EAST);
+        jpnC.add(jlbM);
+        jpnC.add(jcb);
+        jpnC.add(jlbK);
+        jpnC.add(jpf);
+        jpnC.add(jbtnE);
+        jpnC.add(jbtnD);
+        jpnC.add(jbtnR);
+        jpnC.add(jbtnC);
+        jpnL.add(jsL);
+        jpnR.add(jsR);
+        BG.add(jbtnE);
+        BG.add(jbtnD);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Success sc = new Success();
+                sc.setVisible(true);
+            }
+        });
     }
-}
+    }
