bec3469f991b07e172a32984041eff28bcd8272f
106021152 <32092982+106021152@users.noreply.github.com>
2018-10-11 11:39:38 +0800

    Update Encrypt.java

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -10,8 +11,8 @@ public class Encrypt extends JFrame {
     private JTextArea jaR = new JTextArea("",30,15);
     private JScrollPane jsL = new JScrollPane(jaL);
     private JScrollPane jsR = new JScrollPane(jaR);
-    private JPasswordField jpf = new JPasswordField();
-    private String [] methodstr = {"Caesar","Xor"};
+    private JTextField jtf = new JTextField();
+    private String [] methodstr = {"DES","AES","Caesar","XOR"};
     private JComboBox jcb = new JComboBox(methodstr);
     private JRadioButton jbtnE = new JRadioButton("Encrypt");
     private JRadioButton jbtnD = new JRadioButton("Decrypt");
@@ -35,7 +36,7 @@ public class Encrypt extends JFrame {
         jpnC.add(jlbM);
         jpnC.add(jcb);
         jpnC.add(jlbK);
-        jpnC.add(jpf);
+        jpnC.add(jtf);
         jpnC.add(jbtnE);
         jpnC.add(jbtnD);
         jpnC.add(jbtnR);
@@ -44,6 +45,83 @@ public class Encrypt extends JFrame {
         jpnR.add(jsR);
         BG.add(jbtnE);
         BG.add(jbtnD);
+        jbtnR.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(jbtnE.isSelected()) {
+                    int dataLength = jaL.getText().length();
+                    if(dataLength > 0){
+                        switch (jcb.getSelectedIndex()){
+                            case 0:
+                                JOptionPane.showMessageDialog(Encrypt.this,"sorry,"+jcb.getSelectedItem()+"not implement yet!");
+                                break;
+                            case 1:
+                                JOptionPane.showMessageDialog(Encrypt.this,"sorry,"+jcb.getSelectedItem()+"not implement yet!");
+                                break;
+                            case 2:
+                                try {
+                                int key = Integer.parseInt(jtf.getText());
+                                char data [] = jaL.getText().toCharArray();
+                                for(int i = 0 ; i < dataLength; i++){
+                                    data[i] = (char) (data[i]+ key);
+                                }
+                                    jaR.setText(new String(data));
+                            }catch (NumberFormatException exp){
+                                JOptionPane.showMessageDialog(Encrypt.this,"key is not a Number");
+                            }catch (Exception exp1){
+                                    JOptionPane.showMessageDialog(Encrypt.this,"Error:"+exp1.toString());
+                                }
+                                break;
+                            case 3:
+                                JOptionPane.showMessageDialog(Encrypt.this,"sorry,"+jcb.getSelectedItem()+"not implement yet!");
+                                break;
+                        }
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(Encrypt.this,"sorry,"+jcb.getSelectedItem()+"not implement yet!");
+                    }
+                }
+                else if(jbtnD.isSelected()) {
+                    int dataLength = jaR.getText().length();
+                    if(dataLength > 0){
+                        switch (jcb.getSelectedIndex()){
+                            case 0:
+                                JOptionPane.showMessageDialog(Encrypt.this,"sorry,"+jcb.getSelectedItem()+"not implement yet!");
+                                break;
+                            case 1:
+                                JOptionPane.showMessageDialog(Encrypt.this,"sorry,"+jcb.getSelectedItem()+"not implement yet!");
+                                break;
+                            case 2:
+                                try {
+                                    int key = Integer.parseInt(jtf.getText());
+                                    char data [] = jaR.getText().toCharArray();
+                                    for(int i = 0 ; i < dataLength; i++){
+                                        data[i] = (char) (data[i] - key);
+                                    }
+                                    jaL.setText(new String(data));
+                                }catch (NumberFormatException exp){
+                                    JOptionPane.showMessageDialog(Encrypt.this,"key is not a Number");
+                                }catch (Exception exp1){
+                                    JOptionPane.showMessageDialog(Encrypt.this,"Error:"+exp1.toString());
+                                }
+                                break;
+                            case 3:
+                                JOptionPane.showMessageDialog(Encrypt.this,"sorry,"+jcb.getSelectedItem()+"not implement yet!");
+                                break;
+                        }
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(Encrypt.this,"sorry,"+jcb.getSelectedItem()+"not implement yet!");
+                    }
+                }
+            }
+        });
+        jbtnC.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -52,4 +130,5 @@ public class Encrypt extends JFrame {
             }
         });
     }
-    }
+}
+
